bbc4b6267d5b33a2284011843c48fafecd7db98e
AleReeal <alejandrepixel@gmail.com>
2024-05-09 09:43:44 +0200

    tolto sqlite3 dal codice e dai moduli

--- curriculum/src/index.js
+++ curriculum/src/index.js
@@ -1,35 +1,8 @@
 const express = require("express")
 const ejs = require("ejs")
-const sql = require("better-sqlite3")
-const Database = require("better-sqlite3/lib/database")
 const app = express()
 
 //Funzioni
-
-function eseguiSelect(query){
-    const db = new Database('./database/sito.db')
-    const risultato = db.prepare(query).all()
-    return risultato
-}
-
-function ordinaTabella(ra){
-    const raw = ra
-    const consegnato = []
-    let cont = 0
-    while(raw.lenght>0){
-        let num = raw.findIndex(ele => ele.valore == cont)
-        consegnato.push(raw[num])
-        raw = raw.toSpliced(num, 1)
-        if(cont < 2){
-            cont++
-        }
-        else{
-            cont = 0
-        }
-        raw=[{competenza:"ciao"}]
-    }
-    return raw
-    }
 
 app.use(express.static(__dirname))
 app.set("view engine", "ejs")
@@ -39,8 +12,7 @@ app.get("/", (req, res)=>{
 })
 
 app.get("/home", (req, res)=>{
-    const risultato = ordinaTabella(eseguiSelect("SELECT * FROM competenze;"))
-    res.render("home", { competenze: risultato})
+    res.render("home", { competenze: "SQL"})
 })
 
 app.get("/portfolio", (req,res)=>{
